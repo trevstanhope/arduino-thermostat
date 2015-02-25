f43d3cc8dd6ecbbc8758ae0f6f27160b2e66500d
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-24 21:21:33 -0500

    should there be a analog read to adjust the setpoint?

--- sketches/ThermostatPID/ThermostatPID.ino
+++ sketches/ThermostatPID/ThermostatPID.ino
@@ -17,6 +17,7 @@ const int DHT_PIN = 3;
 const int SERVO_PIN = 9;
 const int DHT_TYPE = 22;
 const int BAUD = 9600;
+const int IDEAL_TEMP = 20;
 
 /* --- Variables --- */
 double Setpoint, Input, Output;
@@ -27,8 +28,8 @@ Servo servo;
 void setup() {
   
   /* --- PID --- */
-  Input = 
-  Setpoint = 100;
+  Input = IDEAL_TEMP;
+  Setpoint = IDEAL_TEMP;
   myPID.SetMode(AUTOMATIC);
 
   /* --- I/O --- */
@@ -38,6 +39,7 @@ void setup() {
 }
 
 void loop() {
+  
   Input = dht.readTemperature();
   myPID.Compute();
   servo.write(Output);
